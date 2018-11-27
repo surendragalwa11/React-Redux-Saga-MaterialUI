e87cd88b34888a709107a06a11e8e24143742333
Surendra Galwa <43715945+surendragalwa111@users.noreply.github.com>
2018-11-27 11:05:59 +0530

    Removed debugger console.log()

--- src/components/EditRowForm.jsx
+++ src/components/EditRowForm.jsx
@@ -6,30 +6,15 @@ class EditRowForm extends Component
     constructor(props)
     {
         super(props);
-        console.log("Constructor of EditRowForm called");
-        console.log(props);
-        console.log(props.item);
-        console.log(props.item.id);
         this.state={
             fname:props.item ? props.item.first_name : '',
             lname:props.item ? props.item.last_name : '',
             iurl:props.item ? props.item.avatar : ''
         };
-        console.log(this.state.fname);
     }
 
     
-   onEditClick=()=>{
-        console.log("onEditClick called");
-        return this.props.item;
-   }
-
-    
     onSubmit=()=>{
-    
-        console.log("onSubmit of EditROw called");
-        console.log(this.props.item.id);
-        console.log(this.state);
         this.props.onSubmit({
             "data":[{ 
                 "id":this.props.item.id, 
@@ -43,29 +28,20 @@ class EditRowForm extends Component
 
     onFnameChange=(e)=>{
         const Fname=e.target.value;
-        console.log("onFnameChange in EditRowForm, called");
-        console.log(Fname);
         this.setState({fname:Fname});    
     };
     onLnameChange=(e)=>{
         const Lname=e.target.value;
-        console.log("onLnameChange in EditRowForm, called");
-        console.log(Lname);
         this.setState({lname:Lname});   
     };
     
     onIurlChange=(e)=>{
         const Iurl=e.target.value;
-        console.log("onIurlChange in EditRowForm, called");
-        console.log(Iurl);
         this.setState({iurl:Iurl});    
     };
 
     render()
     {
-        console.log(this.props);
-        console.log(this.state);
-        console.log("We've called EditRowForm component");
         return(
         <div>
             
