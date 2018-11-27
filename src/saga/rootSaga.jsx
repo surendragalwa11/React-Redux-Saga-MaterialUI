7902e76ad79df26b14475cc97b837b2f22dae0b1
Surendra Galwa <43715945+surendragalwa111@users.noreply.github.com>
2018-11-27 11:13:22 +0530

    Removed debugger console.log()

--- src/saga/rootSaga.jsx
+++ src/saga/rootSaga.jsx
@@ -1,32 +1,24 @@
 import {call,put,takeEvery} from 'redux-saga/effects';
-//import {takeEvery} from 'redux-saga';
 import { startFetchAPIRequest, startFetchAPISuccess, startFetchAPIFailure } from '../actions/addEdit';
 
 export default function* rootSaga()
 {
-    console.log("Watcher saga is running");
     yield takeEvery('FETCHED_ITEM', fetchAPI);
 }
   
 function* fetchAPI()
 {
-    console.log("controls came to fetchAPI saga from watcher saga");  
     try
     {
-        console.log("in try block of root saga");
         yield put(startFetchAPIRequest());
         const result = yield call(() => {
             return fetch('https://reqres.in/api/users')
                    .then(res => res.json())
         });
-        console.log(result);
         yield put(startFetchAPISuccess(result));
-              
-        console.log(result);
     } 
     catch (error)
     {
-        console.log("In catch/error block of root saga");
         yield put(startFetchAPIFailure());
     }
 }
