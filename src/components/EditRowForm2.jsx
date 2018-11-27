9e422336c042d6410ef92f6b4d5b846cc398f0cb
Surendra Galwa <43715945+surendragalwa111@users.noreply.github.com>
2018-11-27 11:08:13 +0530

    Removed debugger console.log()

--- src/components/EditRowForm2.jsx
+++ src/components/EditRowForm2.jsx
@@ -14,9 +14,6 @@ class AddRowForm extends Component
     constructor(props)
     {
         super(props);
-        console.log("Constructor of AddRowForm called");
-        console.log(props);
-        
         this.state={
             open:false
         };
@@ -24,7 +21,6 @@ class AddRowForm extends Component
     
     onSubmit=(e)=>{
         this.setState({ open: false });
-    
         this.props.onSubmit();
     }
     
@@ -40,28 +36,20 @@ class AddRowForm extends Component
     
     onFNChange=(e)=>{
         const Fname=e.target.value;
-        console.log("onFnameChange in AddRowForm, called");
-        console.log(Fname);
         this.setState({fname:Fname});    
     };
     onLNChange=(e)=>{
         const Lname=e.target.value;
-        console.log("onLnameChange in AddRowForm, called");
-        console.log(Lname);
         this.setState({lname:Lname});   
     };
     
     onIUChange=(e)=>{
         const Iurl=e.target.value;
-        console.log("onIurlChange in AddRowForm, called");
-        console.log(Iurl);
         this.setState({iurl:Iurl});    
     };
 
     render()
     {
-        console.log(this.props);
-        console.log("We've called Add RowForm component");
         return(
             <div >
                 <Button variant="extendedFab" color="primary"
@@ -127,71 +115,3 @@ class AddRowForm extends Component
 
 
 export default AddRowForm;
-
-
-
-// import React,{Component} from 'react';
-
-// class EditRowForm2 extends Component
-// {
-//     render()
-//     {
-//         return(<div>
-//             <div onClick={this.props.onEditClick}>
-//                 <button type="button" className="btn btn-info btn-lg" 
-//                     data-toggle="modal" data-target="#myModal"
-//                 >
-//                     Edit
-//                 </button>
-//            </div>
-
-             
-//             <div className="modal fade" id="myModal" role="dialog">
-//                 <div className="modal-dialog">
-//                     <div className="modal-content">
-//                         <div className="modal-header">
-//                             <button type="button" className="close" data-dismiss="modal">&times;</button>
-//                             <h4 className="modal-title">Modal Header</h4>
-//                         </div>
-//                    <div className="modal-body">
-//                         <div className="container">
-//                             <h2>Edit entry</h2>
-//                             <form  className="form-inline">
-                       
-//                                 First name <br />
-//                                 <input type="text" defaultValue={this.props.fname}
-//                                     onChange={this.props.onFnameChange}
-                                     
-//                                 /> <br />
-                    
-//                                 Last name <br />
-//                                 <input type="text" defaultValue={this.props.lname}
-//                                     onChange={this.props.onLnameChange} 
-                                    
-//                                 /> <br />
-
-//                                 Image url<br />
-//                                 <input type="text" defaultValue={this.props.iurl}
-//                                     onChange={this.props.onIurlChange} 
-                                    
-//                                 /> <br /> <br />
-                                
-//                                 <button onClick={ this.props.onSubmit } data-dismiss="modal"> 
-//                                     Submit 
-//                                 </button>
-                         
-//                            </form>
-//                        </div>
-//                    </div>
-//                    <div className="modal-footer">
-//                         <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-//                    </div>
-//                 </div>
-//             </div>
-//         </div>
-//    </div>
-//         )
-//     }
-// }
-
-// export default EditRowForm2;
