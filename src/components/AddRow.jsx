b0715778e676a7d9d0f52b8ee94d30ed28328966
Surendra Galwa <43715945+surendragalwa111@users.noreply.github.com>
2018-11-27 10:58:41 +0530

    Removed debugger console.log()

--- src/components/AddRow.jsx
+++ src/components/AddRow.jsx
@@ -8,27 +8,14 @@ import AddRowForm from './AddRowForm';
 class AddRow extends Component
 {
     onSubmit=(item)=>{
-        console.log("onSubmit of AddRow called.");
-        console.log(item);
         this.props.addItem(item);
     }
     
     render()
     {
-        console.log("AddRow rendered,Rendering===AddRowForm");
         return(<AddRowForm onSubmit={this.onSubmit} />);
     }
 }
-
-// const mapDispatchToProps=(dispatch)=>({
-// addItem:(item)=>dispatch(addItem(item))
-// });
-
-// const mapDispatchToProps = (dispatch) => {
-//     console.log("AddItem: mapping dispatch to props");
-//     return  (item) => dispatch(addItem(item));
-//     // return (item)=>dispatch(fetchItem());
-//   };
 
 const mapDispatchToProps={
 addItem:addItem
