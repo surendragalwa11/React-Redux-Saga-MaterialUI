747b34352f8e77a1b09122ffef38fd7e540b5e31
Surendra Galwa <43715945+surendragalwa111@users.noreply.github.com>
2018-11-27 11:09:57 +0530

    Removed debugger console.log()

--- src/container/DisplayPageContainer.jsx
+++ src/container/DisplayPageContainer.jsx
@@ -9,7 +9,7 @@ class DisplayPageContainer extends Component
 {
         componentDidMount()
         {
-                this.props.dispatch(fetchItem())  
+            this.props.dispatch(fetchItem())  
         }
         
         render()
@@ -28,70 +28,12 @@ class DisplayPageContainer extends Component
 }
 
 const mapStateToProps=(state)=>{
-        console.log("DisplayPageContainer:mapStateToProps called");
-        console.log(state);
-        console.log({items:ItemSelector(state.items)});
-        console.log({items:state.items,isLoading:state.isLoading,error:state.error});
         return {
                 items:ItemSelector(state.items),
                 isLoading:state.isLoading,
                 error:state.error
         };
 
-        // return {
-        //         //items:startFetchAPISuccess(state.items.data)
-        //         items:state.items,
-        //         isLoading:state.isLoading,
-        //         error:state.error
-        // };
 }
 
 export default connect(mapStateToProps)(DisplayPageContainer);
-
-
-//////----------/////////
-// const DisplayPageContainer=(props)=>{
-    
-//         console.log("we've reached inside DisplayPageConatiner");
-//         console.log(props);
-//         console.log(props.items);
-//         console.log("**DisplayPageConatiner:fetchItem action will be called on button click");
-//         //props.dispatch(fetchItem());
-//         return(<div >
-//                 <button type="button" className="btn btn-success"
-//                         onClick={()=>props.dispatch(fetchItem())}>Fetch API</button> 
-
-//                 { props.isLoading ?
-//                      <p>Loading...</p>:
-//                     <div>
-//                         <DisplayComponent items={props.items} />
-//                         <AddRow />
-//                     </div>    
-//                 }
-//         </div>);
-// }
-
-// const mapStateToProps=(state)=>{
-//         console.log("DisplayPageContainer:mapStateToProps called");
-//         console.log(state);
-        
-//         return {
-//                 //items:startFetchAPISuccess(state.items.data)
-//                 items:state.items,
-//                 isLoading:state.isLoading,
-//                 error:state.error
-//         };
-// }
-
-// export default connect(mapStateToProps)(DisplayPageContainer);
-///////////////////////////------------------//////////////
-// export default connect(state=>{
-//         console.log(state);
-//         console.log("DisplayPageContainer--mapping state to props in connect");
-//         return state;
-// })(DisplayPageContainer);
-
-//set isLoading false in reducer to load initial state
-//initial state will be empty
-//after fetching data,we'll set that data to state and will set isLoading:false
-//so that we can see that loaded data in DisplayComponent
